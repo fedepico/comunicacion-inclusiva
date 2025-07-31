2450b88ec0848199e9c508b19bf87135c8d7623c
Federico Gomez <federicogomezch@gmail.comm>
2025-07-31 00:22:47 -0500

    Cleaned app  for Render deployment

--- app.py
+++ app.py
@@ -30,4 +30,5 @@ def voz_a_texto():
     return jsonify({"texto": texto})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
