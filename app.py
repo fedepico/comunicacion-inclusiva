b2ec49c5049807fd6f1aae90088a805f622e37c5
Federico Gomez <federicogomezch@gmail.comm>
2025-08-12 12:52:46 -0500

    Agrego dos

--- app.py
+++ app.py
@@ -1,33 +1,13 @@
-from flask import Flask, request, render_template, jsonify
+from flask import Flask, render_template
 from flask_cors import CORS
-from voz_a_texto import transcribir_audio
-
 import os
-from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 CORS(app)
 
-UPLOAD_FOLDER = "uploads"
-os.makedirs(UPLOAD_FOLDER, exist_ok=True)
-
 @app.route("/")
 def index():
     return render_template("index.html")
-
-@app.route("/voz-a-texto", methods=["POST"])
-def voz_a_texto():
-    if "audio" not in request.files:
-        return jsonify({"error": "No se encontró el archivo de audio"}), 400
-
-    audio_file = request.files["audio"]
-    filename = secure_filename(audio_file.filename)
-    file_path = os.path.join(UPLOAD_FOLDER, filename)
-    audio_file.save(file_path)
-
-    texto = transcribir_audio(file_path)
-
-    return jsonify({"texto": texto})
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
